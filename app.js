ada68f608096231ce9db33e050b9fc4294101028
ValarSiva <valar@konceptslab.com>
2024-08-13 18:19:21 +0530

    CI/CD pipeline

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const express=require('express')
 const app=express()
 
+app.get('/',(req,res)=>{
+    res.send('Welcome to CI/CD pipeline')
+})
+
 app.get('/api',(req,res)=>{
     res.send('Hello World')
 })
